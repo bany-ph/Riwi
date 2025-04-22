f2dd5b1cb36768e0005bb6915aa9b2042184edd6
brisbany <brisbanystudy@gmail.com>
2025-04-22 17:35:42 -0500

    :)

--- Modulo1/purchase_calculator.py
+++ Modulo1/purchase_calculator.py
@@ -1,38 +1,41 @@
-    #Semana 1
+#Semana 1
 
 
-def validation(unit, amount, discount):
-    return not(unit <= 0 or amount <= 0 or discount > 100 or discount < 0)
+def input_validation(message, discount=False):
+   while True:
+        try:
+            value = int(input(message))
+          
+            if discount and (value < 0 or value > 100): #if the value is discount and the value is wrong then 
+                input("Wrong input: discount have to be in the rage of 0 to 100")
+            elif(not discount) and value <= 0: #if the value isn´t discount and value is 0 or negative then
+                input("Wrong input: The input Can't be 0 or negative")
+            else:
+                return value #while stop
+            print("- " * 20) 
+        except ValueError:
+            print("--Wrong input: Try again--")
    
 
 
-def total_no_discount(unit, amount):
-    return unit * amount   
-
-def apply_discount(total, percentage_discount): #Parameters : total of the price without the discount, and the percentage of the discount. 
-    if total == 0:
-        return total
+def total(unit,amount,discount, apply_discount = False):
+    if(apply_discount):
+        return f"{unit * amount - ((unit * amount) * (discount / 100)):.2f}"
     else:
-        return total - (total * (percentage_discount/100))
-
-
-def calculate_total(unit, amount, product_discount):
-
-    return apply_discount(total_no_discount(unit,amount), product_discount)
+        return f"{unit * amount:.2f}"
 
 
 
-while True:
-    try:
-        product_name = input("product name: ")
-        product_amount = (int(input("amount of the product: ")))
-        unit_price = (float(input("unit price: ")))
-        product_discount = (int(input("Product discount: ")))
-        if(validation(unit_price, product_amount, product_discount)): #inputs validations 
-           print(f"{product_name}  total = {calculate_total(unit_price, product_amount,product_discount):.2f}")
-           break
-        else:
-            print("Wrong Inputs")
-        
-    except ValueError:
-        print("Invalid input: try again")        
+product_name = input("product name: ")
+unit_price = input_validation("Unit price of the product: ")
+product_amount = input_validation("Amount of product: ")
+discount = input_validation("Discount: ", True)
+
+print("=" * 35)
+print(f"Name                  {product_name}\n")
+print(f"Unit Price            {unit_price}")
+print(f"Amount                {product_amount}")
+print(f"Total                 {total(unit_price,product_amount,discount)}") # print total without discount
+print(f"Discount              {discount}%")
+print(f"Total with discount   {total(unit_price,product_amount,discount,True)}") #print total with discount
+print("=" * 35)
