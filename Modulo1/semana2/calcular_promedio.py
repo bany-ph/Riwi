b4ae636204dee2ff2a2c4243eac07866ebc27951
brisbany <brisbanystudy@gmail.com>
2025-04-29 11:13:54 -0500

    Update calcular_promedio.py

--- Modulo1/semana2/calcular_promedio.py
+++ Modulo1/semana2/calcular_promedio.py
@@ -8,7 +8,7 @@ Contar calificaciones mayores:
 Verificar y contar calificaciones específicas:
     Permitir al usuario ingresar una lista de calificaciones (separadas por comas)
     Calcular y mostrar el promedio de las calificaciones en la lista
- (NOT FINISH)
+ ()
 '''
 grades:list
 
