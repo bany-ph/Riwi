36df5d67565e2ff0ef489a91cc88e2a28a6dd87d
brisbany <brisbanystudy@gmail.com>
2025-05-01 15:38:48 -0500

    ci

--- Modulo1/semana2/notas.py
+++ Modulo1/semana2/notas.py
@@ -22,55 +22,55 @@ class colors: # para cambiar color de texto de la consola
 
 
 
-
-def grade_by_grade():
-    for i in range(len(grades)):
-        grades[i] = verify_inputs(grades[i])
-
-def verifyInt(n, restart):
+def verify_int(n, restart):
     try:
-        return int(n)
-    except ValueError:
+        return int(n) #string -> int
+    except ValueError: #si n no es un numero
         print(f"{colors.FAIL}El valor no es válido. Tiene que ser un número entero.{colors.ENDC}")
         input("Presiona Enter para Continuar...\n")
         restart()
 
-def verify_inputs(grade):
-    
+def verify_inputs():
+    global grades
+    i:int = 0 #para la lista
     while True:
         try: # in case the input is a letter/word 
-            grade = int(grade)
+            grades[i] = int(grades[i])
 
-            if (grade >= 0 and grade <= 100):
-                return grade
+            if (grades[i] >= 0 and grades[i] <= 100):
+                i+=1
+                if i == len(grades) :
+                    break
+                continue
             
-            grade = input(f"{colors.FAIL}Error: la nota no puede ser menor a 0 y mayor a 100 {colors.ENDC}\n Cambia la nota '{grade}' >")
+
+            grades[i] = input(f"{colors.FAIL}Error: la nota no puede ser menor a 0 o mayor a 100 {colors.ENDC}\n Cambia la nota '{grades[i]}' >")
 
         except ValueError:
-          new_grade = input(f"Error: Cambiar '{grade}' por un numero.{colors.ENDC} \nTienes que ingresar un numero: ")
-          grade = new_grade #change de wrong input to the new one
+          grades[i] = input(f"{colors.FAIL}Error: Cambiar '{grades[i]}' por un numero.{colors.ENDC} \nTienes que ingresar un numero: ")
+      
           print("\n ")
 
 
-def avarage (): #promedio 
+def average (): #promedio 
     global grades
-    sum = 0
+    total:int = 0
     for grade in grades:
-        sum+=grade
-    return sum / len(grades)
+        total += grade
+    return total / len(grades)
 
 
-def highest_grades():
-    grade_check = int(input(f" Mostrar Las notas màs grandes que: "))
+def highest_grades(): #muestra las notas más altas de x numero
+    grade_check = int(input(" Mostrar Las notas màs grandes que: "))
     for grade in grades:
-        if(not grade >= grade_check):
+        if( grade < grade_check):
             continue
         print(colors.OKBLUE + ":" , grade)
 
 def count_grade(): #contar cuántas veces aparece una nota especìfica
  while True:   
     try:
-        grade = int(input(f" Nota que quieres ver cuantas veces se repite: "))
+        grade = int(input(" Nota que quieres ver cuantas veces se repite: "))
         return grades.count(grade)
     except ValueError:
         print(f"{colors.FAIL}ERROR: El valor que ingresaste no se encuentra en la lista o el valor no es un numero {colors.ENDC}" )
@@ -93,25 +93,25 @@ def input_grades():
 def choose():
     print(f"\n{colors.OKBLUE}Your grades: {grades}{colors.ENDC}\n")
     c = input(f"{colors.HEADER}QUE QUIERES HACER:{colors.ENDC} \n1.promedio \n2.calificaciones mayores \n3.contar cuántas veces aparece una nota especìfica\n4.volver\n > ")
-    c = verifyInt(c , choose)
+    c = verify_int(c , choose) #verifica que el input sea el adecuado
     
-    match c:
+    match c: #promedio
         case 1: 
             print(f"\n{colors.HEADER}PROMEDIO{colors.ENDC}\n")
-            print((colors.OKGREEN + "APROBADO" if avarage() > 50 else colors.FAIL + "REPROBADO: " )+ colors.ENDC ,avarage())
+            print((colors.OKGREEN + "APROBADO" if average() > 50 else colors.FAIL + "REPROBADO: " )+ colors.ENDC ,average())
             choose()
-        case 2: 
+        case 2: # calificacines mayores
              print(f"\n{colors.HEADER}NOTAS MAYORES{colors.ENDC}\n")
              highest_grades()
              choose()
-        case 3: 
+        case 3: # Notas repetidas en la lista
             print(f"\n{colors.HEADER}CONTADOR{colors.ENDC}\n")
             print(f"La nota se encuentra: {colors.OKBLUE}{count_grade()}{colors.ENDC} veces")
             choose()
-        case 4: 
+        case 4: #volver al inicio
             main()
         case _: 
-            print("\nError: no existe\n") 
+            print(f"{colors.FAIL}\nError: no existe\n{colors.ENDC}") 
             choose()
 
 
@@ -120,11 +120,11 @@ def main():
     print(f"\n  {colors.HEADER} INICIO {colors.ENDC} \n" )
     keep_going = input("Quieres seguir? (si/no): ").strip()
     if keep_going.lower() == "si"  or keep_going.lower() == "s":
-        input_grades()
-        grade_by_grade()
-        choose()
+        input_grades() #para ingresar las notas
+        verify_inputs()#verificar que las notas esten correctas (numericos y en  el rango de 0-100)
+        choose() #menu de opciones
     else:
-        return
+        return #salir del programa
 
 
 main()
